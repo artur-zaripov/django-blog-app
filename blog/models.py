f1f70158aeffc982053f33a1c1337e853741fc78
Artur Zaripov <artur.zaripov@akvelon.com>
2018-07-10 15:16:26 +0300

    added data models and according migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=2000)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.text
